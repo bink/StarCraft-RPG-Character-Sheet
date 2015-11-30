11986f0b593819ba13df2ad44b8a2d6c71884012
Bink <bink@users.noreply.github.com>
2015-11-30 20:44:04 +0100

    Added validation for more fields.

--- charsheet.js
+++ charsheet.js
@@ -3,11 +3,28 @@ var $ = function(sel)
 	return document.querySelectorAll(sel);
 }
 
-
-function validateAll()
+function getStatField(id)
 {
-	console.log("Validating");
-	var raceSelect = $("#origin")[0];
+	return $("#"+id)[0];
+}
+
+function getStatValue(id)
+{
+	if (!getStatField(id)) return 0;
+	var val = $("#"+id)[0].value;
+	if (parseInt(val) == (typeof NaN))
+	{
+		return val;
+	}
+	else
+	{
+		return parseInt(val);
+	}
+}
+
+function validateRaceAndSpecs()
+{
+	var raceSelect = getStatField("origin");
 	var race = raceSelect.options[raceSelect.selectedIndex].getAttribute("data-race");
 	/**
 	 * Deactivate unavailable specializations
@@ -24,13 +41,36 @@ function validateAll()
 			specs[i].removeAttribute("disabled");
 		}
 	}
+}
 
+function calculateHealth()
+{
+	//var hitpointField = $;
+	//var currentHitpointField = $("#currenthitpoints")[0];
+	var strength = getStatValue("strength");
+	var level = getStatValue("level");
+
+	var hp = 40 + 3.0 * strength + (level - 1) * strength;
+	getStatField("hitpoints").value = hp;
+
+	//healing threshold
+	var ht = Math.floor(0.5 * strength) + 5;
+	getStatField("healingthreshold").value = ht;
+
+	//damage threshold
+	var dt = strength + 12;
+	getStatField("damagethreshold").value = dt;
+}
+
+function validateAll()
+{
+	validateRaceAndSpecs();
+	calculateHealth();
 }
 
 function setupEvents()
 {
 	var inputs = $("input,select");
-	console.log(inputs);
 	for(var i = 0; i < inputs.length; i++)
 	{
 		inputs[i].addEventListener("change",validateAll);
