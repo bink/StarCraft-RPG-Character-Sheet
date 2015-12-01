f929d5dd6c4445232d97b081b19da8592a5b221f
bink <bink@users.noreply.github.com>
2015-12-01 10:13:11 +0100

    Added the remaining origins from the standard ruleset.

--- src/js/tables.js
+++ src/js/tables.js
@@ -16,5 +16,42 @@ var DATA_origins = {
 	"prowess_bonuses":{"melee":1,"pistol":1,"land":1,"mental":1},
 	"adventuring_bonuses":{"lore":1,"influence":1},
 	"other_bonuses":{"credits":250}
+    },
+    "umojan":{
+	"name":"Umojan Protectorate",
+	"race":"t",
+	"point_cost":9,
+	"ability_bonuses":[{"willpower":1,"intelligence":2},{"intelligence":1,"instinct":1}],
+	"prowess_bonuses":{"pistol":1,"land":1,"mental":2},
+	"adventuring_bonuses":{"lore":1,"science":1},
+	"other_bonuses":{}
+    },
+    "kelmorian":{
+	"name":"Kel-Morian Combine",
+	"race":"t",
+	"point_cost":8,
+	"ability_bonuses":[{"strength":2,"agility":1},{"intelligence":1}],
+	"prowess_bonuses":{"melee":1,"durability":1,"land":1},
+	"adventuring_bonuses":{"athletics":1,"endurance":1},
+	"other_bonuses":{}
+    },
+    "colonist":{
+	"name":"Fringe Colonist",
+	"race":"t",
+	"point_cost":12,
+	"ability_bonuses":[{"willpower":1},{"intelligence":1}],
+	"prowess_bonuses":{"melee":1,"pistol":1,"rifle":1,"explosives":1,"durability":1},
+	"adventuring_bonuses":{"acrobatics":1,"endurance":1,"sneak":1},
+	"other_bonuses":{"credits":-250}
+    },
+    "ued":{
+	"name":"United Earth Directorate Expedition",
+	"race":"t",
+	"point_cost":12,
+	"ability_bonuses":[{"instinct":2,"strength":2},{"intelligence":2}],
+	"prowess_bonuses":{"pistol":1,"rifle":1,"land":1,"aerial":1,"defense":1,"mental":1},
+	"adventuring_bonuses":{"tactics":2},
+	"other_bonuses":{}
     }
+
 };
